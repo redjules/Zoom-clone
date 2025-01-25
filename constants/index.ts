9f63fe1813936949ac5176c01d2419ffea3825fd
Ana <anapedrabcn@gmail.com>
2025-01-25 23:02:21 +0100

    fixed new meeting feature

--- constants/index.ts
+++ constants/index.ts
@@ -1,27 +1,36 @@
 export const sidebarLinks = [
   {
-    imgUrl: "/icons/Home.svg",
+    imgURL: "/icons/Home.svg",
     route: "/",
     label: "Home",
   },
+
   {
-    imgUrl: "/icons/upcoming.svg",
+    imgURL: "/icons/upcoming.svg",
     route: "/upcoming",
     label: "Upcoming",
   },
   {
-    imgUrl: "/icons/previous.svg",
+    imgURL: "/icons/previous.svg",
     route: "/previous",
     label: "Previous",
   },
   {
-    imgUrl: "/icons/Video.svg",
+    imgURL: "/icons/Video.svg",
     route: "/recordings",
     label: "Recordings",
   },
   {
-    imgUrl: "/icons/add-personal.svg",
+    imgURL: "/icons/add-personal.svg",
     route: "/personal-room",
     label: "Personal Room",
   },
+];
+
+export const avatarImages = [
+  "/images/avatar-1.jpeg",
+  "/images/avatar-2.jpeg",
+  "/images/avatar-3.png",
+  "/images/avatar-4.png",
+  "/images/avatar-5.png",
 ];
